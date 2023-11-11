2474908306391ac56b1a982c654641e05136c8f2
Melissa <mel.bather@gmail.com>
2023-11-10 17:32:04 -0800

    Update simulation_example1.R
    updated first simulation example

--- simulation_example1.R
+++ simulation_example1.R
@@ -19,7 +19,7 @@ source("functions/generate_session_locations.R")
 source("functions/create_mask.R")
 
 #Create two villages -----------------------------------------------------------
-village_locations1 <- create_villages(2, c(-200, 500), c(-300, 250))
+village_locations1 <- create_villages(2, c(0, 8000), c(0, 700))
 
 #Set number of sessions --------------------------------------------------------
 num_sessions1 <- 3
@@ -42,22 +42,23 @@ single_session_demo <- simulate_capture_histories_with_sessions(
   alpha_forest = 400, 
   alpha_protected = 300, 
   alpha_altitude = 400, 
-  beta0 = -1, beta1 = 0.0001, 
+  beta0 = -1, beta1 = -0.0002, 
   mask_locations = mask1, 
   detector_locations = detectors1, 
   g0_base = 0.85, 
   sigma = 85, 
   x_range = c(sessions1[1,5], sessions1[1,6]), 
   y_range = c(sessions1[1,7], sessions1[1,8]), 
-  beta2 = -0.0002, beta3 = 1.6, beta4 = 0.0001, 
-  beta5 = 0.4, beta6 = -0.2, beta7 = 1.8, 
-  beta8 = 0.00001, beta9 = 0.000001,
+  beta2 = -0.0002, beta3 = 2.5, beta4 = 0.00015, 
+  beta5 = 0.5, beta6 = -0.2, beta7 = 1.9, 
+  beta8 = -0.00001, beta9 = 0.000002,
   village_locations = village_locations1, 
-  cp = 50, cp_beta0 = log(50), cp_beta1 = -1)
+  cp_beta0 = log(50), cp_beta1 = -1)
 
 
 #Run simulation with all sessions ----------------------------------------------
 session_sim_capt_hist <- vector(mode = "list", length = num_sessions1)
+session_sim_bin_capt_hist <- vector(mode = "list", length = num_sessions1)
 session_animal_locations <- vector(mode = "list", length = num_sessions1)
 session_sim_forest <- vector(mode = "list", length = num_sessions1)
 session_sim_altitude <- vector(mode = "list", length = num_sessions1)
@@ -71,19 +72,20 @@ for(i in 1:num_sessions1) {
     alpha_forest = 400, 
     alpha_protected = 300, 
     alpha_altitude = 400, 
-    beta0 = -1, beta1 = 0.0001, 
+    beta0 = -1, beta1 = 0.0002, 
     mask_locations = mask1, 
     detector_locations = detectors1, 
     g0_base = 0.85, 
     sigma = 85, 
     x_range = c(sessions1[i,5], sessions1[i,6]), 
     y_range = c(sessions1[i,7], sessions1[i,8]), 
-    beta2 = -0.0002, beta3 = 1.6, beta4 = 0.0001, 
-    beta5 = 0.4, beta6 = -0.2, beta7 = 1.8, 
-    beta8 = 0.00001, beta9 = 0.000001,
+    beta2 = -0.0002, beta3 = 2.5, beta4 = 0.00015, 
+    beta5 = 0.5, beta6 = -0.2, beta7 = 1.9, 
+    beta8 = -0.00001, beta9 = 0.000002,
     village_locations = village_locations1, 
-    cp = 50, cp_beta0 = log(50), cp_beta1 = -1)
+    cp_beta0 = log(50), cp_beta1 = -1)
   session_sim_capt_hist[[i]] <- session_sim$long_form_capture_history
+  session_sim_bin_capt_hist[[i]] <- session_sim$binary_capture_history
   session_animal_locations[[i]] <- session_sim$animal_locations
   session_sim_forest[[i]] <- session_sim$forest
   session_sim_altitude[[i]] <- session_sim$altitude
@@ -94,6 +96,7 @@ for(i in 1:num_sessions1) {
 
 all_animal_locations1 <- do.call(rbind, session_animal_locations)
 all_capt_hist1 <- do.call(rbind, session_sim_capt_hist)
+all_bin_capt_hist1 <- do.call(rbind, session_sim_bin_capt_hist)
 all_forest1 <- do.call(rbind, session_sim_forest)
 all_altitude1 <- do.call(rbind, session_sim_altitude)
 all_protected1 <- do.call(rbind, session_sim_protected)
